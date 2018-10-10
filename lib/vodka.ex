e12513eeda887b9d44ea5b1b2602c0eb528cf486
gregoire <gregoire.loens59670@gmail.com>
2018-10-10 16:24:01 +0200

    unable main function

--- lib/vodka.ex
+++ lib/vodka.ex
@@ -11,10 +11,4 @@ defmodule Vodka do
   def generate_access_token do
     Goth.Token.for_scope("https://www.googleapis.com/auth/cloud-platform")
   end
-
-  def main(args) do
-    start()
-    {:ok, token} = generate_access_token()
-    IO.inspect(Vodka.Query.send_query(token, "start", "fr"))
-  end
 end

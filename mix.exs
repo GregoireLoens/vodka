b4db4c0aac53209d9fc443a155e09bf4e77c29cb
Grégoire.loens <gregoire.loens59670@gmail.com>
2019-05-01 14:08:58 +0200

    update dep

--- mix.exs
+++ mix.exs
@@ -6,8 +6,12 @@ defmodule Vodka.MixProject do
       app: :vodka,
       version: "0.2.0",
       elixir: "~> 1.6",
-      escript: [main_module: Vodka],
+      escript: [
+        main_module: Vodka
+      ],
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -20,6 +24,7 @@ defmodule Vodka.MixProject do
     ]
   end
 
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
@@ -29,5 +34,22 @@ defmodule Vodka.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "vodka",
+      # These are the default files included in the package
+      files: ~w(lib mix.exs README*),
+      licenses: ["Apache 2.0"],
+      links: %{
+        "Gitlab" => ""
+      }
+    ]
+  end
+
+  defp description() do
+    "Dialogflow SDK in elixir "
   end
 end
